ed9d190c126708bbeb4d57b0dcfd4313e01854f7
Shibin B Shaji <shibinbshaji007@gmail.com>
2021-10-06 10:24:28 +0530

    can enter text now, added figlet for fun

--- ale.py
+++ ale.py
@@ -1,18 +1,37 @@
+import pyfiglet
+import argparse
 import json
 from random import sample
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--Stealth",help = "Open without Banner",action='store_true')
+parser.add_argument("-m", "--Message", help = "Insert Text")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+if (args.Stealth == False):
+	result = pyfiglet.figlet_format("lul", font = "isometric1" )
+	print(result)
+else:
+	print("lul")
 
 with open('places.json', 'r') as file:
     places_dict = json.loads(file.read())
 
-random_text = 'hello world'.upper()
+random_text = args.Message.upper()
+##random_text = 'hello world'.upper()
+
+##print(random_text)
+##print(type(random_text))
 
 new_text = ''
 for char in random_text:
     try:
-        new_text += sample(place_dict[char],1)[0]
+        new_text += sample(places_dict[char],1)[0]
     except KeyError:
         pass
     new_text += ' '
-
 	
 print(new_text)
